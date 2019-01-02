53d82b9c1d70160f18be9b9feda08e4e8b7e151b
penghaitao <penghaitao918@163.com>
2019-01-02 17:43:50 +0800

    增加notifyDataSetChanged

--- library/src/main/java/com/uniquext/android/banner/BannerPlayer.java
+++ library/src/main/java/com/uniquext/android/banner/BannerPlayer.java
@@ -6,9 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.FrameLayout;
 
 import static com.uniquext.android.banner.BannerHandler.MSG;
@@ -88,6 +85,13 @@ public class BannerPlayer extends FrameLayout {
     public void setBannerAdapter(BannerAdapter bannerAdapter) {
         mViewPager.setAdapter(new BannerPagerAdapter(bannerAdapter));
         mViewPager.addOnPageChangeListener(new BannerPageChangeListener(mViewPager, bannerAdapter));
+
+    }
+
+    public void notifyDataSetChanged() {
+        if (mViewPager.getAdapter() != null) {
+            mViewPager.getAdapter().notifyDataSetChanged();
+        }
     }
 
     @Override
